be20726e06665794069fa220608dec1414bd4cf1
Gabriel Laroche <gabriel.laroche@nurun.com>
2019-07-25 14:37:21 -0400

    Added Word and Character count, started title case conversion

--- case-converter.js
+++ case-converter.js
@@ -7,6 +7,9 @@ const capitalizedCaseBtn = document.getElementById('capitalizedCaseBtn');
 const copyTextBtn = document.getElementById('copyBtn');
 const clearTextBtn = document.getElementById('clearBtn');
 
+const numCharsElem = document.getElementById('numChars');
+const numWordsElem = document.getElementById('numWords');
+
 const toUpperCase = () => {
     textarea.value = textarea.value.toUpperCase();
 };
@@ -37,6 +40,33 @@ const toCapitalizedCase = () => {
     textarea.value = capitalizeChars(' ');
 };
 
+const toTitleCase = () => {
+    /***
+     * WHAT TO CAPITALIZE :
+     ** First word
+     ** Last Word
+     ** Words >= 5 chars
+     ** Pronouns (I, Me, We, Us, You, She, Her, He, Him, It, They, Them,)
+     * */
+
+    const importantShortWords = [
+        "i", "me", "we", "us", "you", "she", "her", "he", "him", "it", "they", "them",
+        "this", "that", "who", "whom", "all", "any", "both", "few", "many", "most", "none",
+        "many", "none", "some", "what", "my", "your", "his", "her", "its", "our", "ours", "mine",
+        "hers", "as", "even", "if", "than", "that", "when", "why"
+    ]
+};
+
+const countChars = () => {
+    numCharsElem.innerText = textarea.value.length.toString();
+};
+
+const countWords = () => {
+    const textContent = textarea.value.split(" ");
+
+    numWordsElem.innerText = textarea.value === "" ? "0" : textContent.length.toString();
+};
+
 const copyToClipboard = () => {
     if (textarea.value !== "") {
         textarea.select();
@@ -56,6 +86,8 @@ const init = () => {
 
     clearTextBtn.addEventListener('click', clearTextarea);
     copyTextBtn.addEventListener('click', copyToClipboard);
+    textarea.addEventListener('input', countChars);
+    textarea.addEventListener('input', countWords);
 };
 
 (function () {
